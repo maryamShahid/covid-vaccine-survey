b4d485db64ebd9cef8f715cdc75c6e2c362cb49a
Maryam Shahid <maryamshahid673@gmail.com>
2022-03-28 00:40:00 +0300

    updated send functionality

--- app/src/main/java/com/example/covid19vaccinesurvey/MainActivity.java
+++ app/src/main/java/com/example/covid19vaccinesurvey/MainActivity.java
@@ -6,16 +6,25 @@ import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvDate;
-    EditText etDate;
+    EditText dateOfBirth;
+    EditText nameSurname;
+    EditText city;
+    EditText gender;
+    EditText vaccineType;
+    EditText sideEffects;
+    EditText casesAndSymptoms;
+    Button buttonSend;
     DatePickerDialog.OnDateSetListener setListener;
 
     @Override
@@ -23,7 +32,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvDate = findViewById(R.id.tv_date);
+        dateOfBirth = findViewById(R.id.tv_date);
+        nameSurname = findViewById(R.id.name_surname);
+        city = findViewById(R.id.city);
+        gender = findViewById(R.id.gender);
+        vaccineType = findViewById(R.id.vaccine_type);
+        sideEffects = findViewById(R.id.side_effects);
+        casesAndSymptoms = findViewById(R.id.cases_and_symptoms);
+        buttonSend = findViewById(R.id.send);
+
+        nameSurname.addTextChangedListener(enterTextWatcher);
+        city.addTextChangedListener(enterTextWatcher);
+        gender.addTextChangedListener(enterTextWatcher);
+        vaccineType.addTextChangedListener(enterTextWatcher);
+        sideEffects.addTextChangedListener(enterTextWatcher);
+        casesAndSymptoms.addTextChangedListener(enterTextWatcher);
+        dateOfBirth.addTextChangedListener(enterTextWatcher);
+
         // etDate = findViewById(R.id.et_date);
 
         Calendar calendar = Calendar.getInstance();
@@ -32,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        tvDate.setOnClickListener(new View.OnClickListener() {
+        dateOfBirth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
@@ -48,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month + 1;
                 String date = day + "/" + month + "/" + year;
-                tvDate.setText(date);
+                dateOfBirth.setText(date);
             }
         };
 
@@ -68,4 +93,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
     }
+    private TextWatcher enterTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String userName = nameSurname.getText().toString().trim();
+            String userCity = city.getText().toString().trim();
+            String userGender = gender.getText().toString().trim();
+            String userVaccine = vaccineType.getText().toString().trim();
+            String userSideEffect = sideEffects.getText().toString().trim();
+            String userCaseNSymptom = casesAndSymptoms.getText().toString().trim();
+            String userDate = dateOfBirth.getText().toString().trim();
+
+            buttonSend.setEnabled(!userName.isEmpty() && !userCity.isEmpty() && !userGender.isEmpty() &&
+                    !userVaccine.isEmpty() && !userSideEffect.isEmpty() && !userCaseNSymptom.isEmpty() && !userDate.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
